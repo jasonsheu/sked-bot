8235c302aeaa3fe8481926e1c5d3cfa9335c4862
Jason Sheu <jasonsheu27@gmail.com>
2019-06-15 19:54:56 -0700

    updated bot

--- src/sked_bot.py
+++ src/sked_bot.py
@@ -9,11 +9,14 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         # we do not want the bot to reply to itself
+        print(message.content, message.author)
         if message.author.id == self.user.id:
             return
 
+
         if message.content.startswith('!hello'):
             await message.channel.send('Hello {0.author.mention}'.format(message))
+
 
 client = MyClient()
 client.run('NTg5NjQyNzIzOTM0NDcwMjA0.XQWpow.jMiLZPd5J5lax6ZkMWHONmTXVhs')
